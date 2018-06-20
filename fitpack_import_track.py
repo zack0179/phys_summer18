c61c798c6a4da3645c826f6dc0aa6e923f725882
zack0179 <zack0179@gmail.com>
2018-06-20 12:27:24 -0400

    tracker update

--- fitpack_import_track.py
+++ fitpack_import_track.py
@@ -6,36 +6,64 @@ Created on Thu Jun 14 18:33:00 2018
 """
 from __future__ import print_function
 import os
-import numpy as np
+
+
+def yada(path):
+    imports = {}
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if not file.endswith(".py"):
+                continue
+            parent_file = os.path.join(root, file)
+            imports[parent_file] = []
+            with open(parent_file) as file:
+                file_lines = file.readlines()
+            extras = []
+            while file_lines:
+                for line in file_lines:
+                    words = line.strip().split()
+                    if len(words) < 2:
+                        continue
+                    first, second, *rest = words
+                    if first == "from":
+                        if "as" in rest:
+                            edge = " ".join(rest[1:rest.index("as")])
+                        elif "#" in rest:
+                            edge = " ".join(rest[1:rest.index("#")])
+                        else:
+                            edge = "".join(rest[1:]).replace(",", ", ")
+                        imports[parent_file].append([second,
+                                                     {"edge":
+                                                         edge}
+                                                     ])
+                    elif first == "import":
+                        for module in second.split(","):
+                            imports[parent_file].append(module
+                                                        .replace(";", "")
+                                                        .replace("#", ""))
+                    rest = " ".join(rest)
+                    if ";" in rest:
+                        extras += rest.split(";")[1:]
+                file_lines = extras.copy()
+                extras = []
+
+    # Show your work
+    for key in imports.keys():
+        print(key)
+        for val in imports[key]:
+            if type(val) is list:
+                for v in val:
+                    if type(v) is dict:
+                        print("        edge: " + v["edge"])
+                    else:
+                        print("    " + v)
+            else:
+                print("    " + val)
+
+    return imports
 
 
 if __name__ == "__main__":
-    os.chdir("/home/zes5027/GIT/phys_summer18")
-    path = "/home/zes5027/GIT/fitpack"
-    
-    fitpack_track = {}
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.endswith(".py"):
-                parent_file = os.path.join(root, file)
-                target_dependence = []
-                for line in open(parent_file):
-                    if "from" in line.split():
-                        marks = line.split()[1]
-                        if "," in marks:
-                            for x in marks.split(","):
-                                target_dependence.append(x)
-                        else:
-                            target_dependence.append(marks)
-                    elif "import" in line.split() and "from" not in line.split():
-                        marks = line.split()[1]
-                        if "," in marks:
-                            for x in marks.split(","):
-                                target_dependence.append(x)
-                        else:
-                            target_dependence.append(marks)
-                fitpack_track[parent_file] = target_dependence
-    
-    # Save
-    np.save('fitpack_track.npy', fitpack_track) 
-    
+    path = r"C:\Users\Aardvark\Documents\GIT\fitpack"
+
+    imports = yada(path)
