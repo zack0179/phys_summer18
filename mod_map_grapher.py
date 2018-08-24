80248a894c6aa8f85c2f3fafab93c4f0121170f9
mason <14malbright@gmail.com>
2018-08-23 21:03:14 -0400

    mod_map_grapher essentially complete

--- mod_map_grapher.py
+++ mod_map_grapher.py
@@ -10,34 +10,52 @@ DEFAULT_EDGE_COLOR = "#000102"
 DEFAULT_FONT_COLOR = "#b9cdfb"
 DEFAULT_FIG_COLOR = "#2e4272"
 
-dir_path = r"/home/mda5232/git/fitpack"
+dir_path = r"/home/mason/dev/fitpack"
 
-exclude = {"examples"}
 ignore = {"argparse", "collections", "cPickle", "fnmatch", "numba", "numpy",
           "os", "pandas", "pickle", "psutil", "pylab", "re", "scipy", "sys",
           "timeit", "warnings", "zlib", "zmq"}
 
 deps = find_dependencies(dir_path, {"jam3d", "mcproc", "run_notebook"})
 
-mods = [k.split("/")[-1].split(".")[0] for k in deps.keys()]
+deps = {f[1:].replace(".py", "").replace("/", "."): imports
+        for f, imports in deps.items()}
+
+mods = [k.split(".")[-1] for k in deps.keys()]
 
 ambigs = {k: v for k, v in Counter(mods).items() if v > 1}
 
+mods = set(mods)
+
 G = nx.DiGraph()
 
+dep_ups = {".".join(x.split(".")[:-1]) for x in deps if "." in x}
+
 for f, imports in deps.items():
-    parts = f.split("/")[1:]
-    parts[-1] = parts[-1].split(".")[0]
-    f = ".".join(parts)
     for big, smalls in imports:
-        if not (ignore & set(big.split("."))):
-            G.add_edge(f, big, info=smalls)
+        if (ignore & set(big.split("."))):
+            continue
+
+        f_parts = f.split(".")
+
+        while f_parts:
+            if big in deps or big in dep_ups:
+                break
+            big = f_parts.pop() + big
+        else:
+            continue
+
+        if smalls and smalls[0] in mods:
+            for small in smalls:
+                G.add_edge(f, big + "." + small)
+        else:
+            G.add_edge(f, big)
 
 
 def plot(node_color=DEFAULT_NODE_COLOR,
          edge_color=DEFAULT_EDGE_COLOR,
          font_color=DEFAULT_FONT_COLOR):
-    fig, ax = plt.subplots(figsize=(8, 6))
+    fig, ax = plt.subplots(figsize=(16, 12))
 
     nx.draw_kamada_kawai(G,
                          with_labels=True,
@@ -55,6 +73,21 @@ def plot(node_color=DEFAULT_NODE_COLOR,
 
 
 if __name__ == "__main__":
-    print(sorted(G.nodes))
+    print("ins")
+    print("=======")
+
+    ins = dict(G.in_degree)
+
+    for node in sorted(ins, key=ins.get):
+        print(node, ":", ins[node])
+
+    print()
+    print("outs")
+    print("=======")
+
+    outs = dict(G.out_degree)
+
+    for node in sorted(outs, key=outs.get):
+        print(node, ":", outs[node])
 
     # fig, ax = plot()
